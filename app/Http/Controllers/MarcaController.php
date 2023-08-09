a543913428eb7f7bcf1169cf894d8845fca0d958
Darlan <darlansilv4@gmail.com>
2023-08-09 00:07:23 -0300

    feat: implementando os primeiros métodos da api (Marca)

--- app/Http/Controllers/MarcaController.php
+++ app/Http/Controllers/MarcaController.php
@@ -7,58 +7,41 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        //
+        $marcas = Marca::all();
+
+        return $marcas->toArray();
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $marca = new Marca();
+        $marca->nome = $request->get('nome');
+        $marca->imagem = $request->get('imagem');
+        $marca->save();
+
+        return $marca->toArray();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
     public function show(Marca $marca)
     {
-        //
+        return $marca->toArray();
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Marca $marca)
     {
-        //
+        $marca->nome = $request->get('nome');
+        $marca->imagem = $request->get('imagem');
+        $marca->save();
+
+        return $marca->toArray();
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Marca  $marca
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Marca $marca)
     {
-        //
+        $marca->delete();
+
+        return ["message" => 'Marca "' . $marca->nome . '" foi removida com sucesso!'];
     }
 }
