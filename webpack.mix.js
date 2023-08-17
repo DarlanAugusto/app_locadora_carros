13b37d58a03ac4711499880845e211d31133cc53
Darlan <darlansilv4@gmail.com>
2023-08-16 21:01:22 -0300

    fix: Corrigindo importação do popper.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('node_modules/@popperjs/core/dist/cjs/popper.js', 'public/js').sourceMaps()
+    .js('node_modules/@popperjs/core/dist/esm/popper.js', 'public/js').sourceMaps()
     .vue()
     .sass('resources/sass/app.scss', 'public/css');
